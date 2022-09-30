01b6fdd03e6cad32dd5592e7b6ae543d65670fec
kechin.vv <kechinv1@rambler.ru>
2022-09-30 17:59:52 +0400

    init

---
+++ src/main/kotlin/repository/LocalRepository.kt
@@ -0,0 +1,36 @@
+package repository
+
+import java.io.File
+import java.io.IOException
+import java.nio.file.*
+import java.nio.file.attribute.BasicFileAttributes
+import java.util.stream.Stream
+
+
+class LocalRepository(val path: Path) {
+
+    @Throws(IOException::class)
+    fun delete() {
+        Files.walkFileTree(path, object : SimpleFileVisitor<Path>() {
+            @Throws(IOException::class)
+            override fun visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult {
+                Files.setAttribute(file, "dos:readonly", false)
+                Files.delete(file)
+                return FileVisitResult.CONTINUE
+            }
+
+            @Throws(IOException::class)
+            override fun postVisitDirectory(dir: Path, exc: IOException): FileVisitResult {
+                Files.delete(dir)
+                return FileVisitResult.CONTINUE
+            }
+        })
+    }
+
+    @get:Throws(IOException::class)
+    val ktStream: Stream<String>
+        get() = Files.walk(path, FileVisitOption.FOLLOW_LINKS)
+            .map { obj: Path -> obj.toFile() }
+            .filter { f: File -> f.isFile && f.name.endsWith(".kt") }.map { obj: File -> obj.absolutePath }
+
+}
---
+++ src/main/kotlin/repository/RemoteRepository.kt
@@ -0,0 +1,17 @@
+package repository
+
+import java.io.IOException
+import java.nio.file.Path
+
+
+class RemoteRepository(var url: String?, var name: String?) {
+
+    @Throws(InterruptedException::class, IOException::class)
+    fun cloneTo(path: Path): LocalRepository {
+        val proc = ProcessBuilder("git", "clone", "--depth=1", "--recurse-submodules", url, path.toString()).start()
+        proc.waitFor()
+        proc.destroy()
+        return LocalRepository(path)
+    }
+
+}

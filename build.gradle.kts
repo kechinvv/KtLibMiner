a709ce7bb78174bd44258ccad5261e5619daca73
kechin.vv <kechinv1@rambler.ru>
2022-11-02 23:39:18 +0400

    kt ver = 1.7.20, import kfg

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,25 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.31"
+    kotlin("jvm") version "1.7.20"
 }
 
 group = "me.valer"
 version = "1.0-SNAPSHOT"
+val kfgVersion = "0.3.5"
 
 repositories {
     mavenCentral()
-    jcenter { url = uri("https://jcenter.bintray.com/") }
+    maven("https://maven.vorpal-research.science")
+    maven{ url = uri("https://jcenter.bintray.com/") }
     google()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.7.20")
     implementation("khttp:khttp:0.1.0")
-    implementation("com.google.code.gson:gson:2.8.6")
-
+    implementation("com.google.code.gson:gson:2.10")
+    implementation("org.vorpal.research:kfg:$kfgVersion")
 }
 
 tasks.test {

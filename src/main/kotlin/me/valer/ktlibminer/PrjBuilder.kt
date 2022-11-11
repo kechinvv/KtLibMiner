ca264f729994b9cf0f19facf1eed57d6ea08a3d2
kechin.vv <kechinv1@rambler.ru>
2022-11-11 21:09:26 +0400

    PrjBuilder init

---
+++ src/main/kotlin/me/valer/ktlibminer/PrjBuilder.kt
@@ -0,0 +1,19 @@
+package me.valer.ktlibminer
+
+import me.valer.ktlibminer.repository.LocalRepository
+import java.nio.file.Path
+
+class PrjBuilder(gradle_path: Path) {
+
+    fun build(prj: LocalRepository) {
+        TODO()
+    }
+
+    fun buildGradle(prj: LocalRepository) {
+        TODO()
+    }
+
+    fun buildMaven(prj: LocalRepository) {
+        TODO()
+    }
+}

956ad743382477468b0941592ac00cb59b8937cc
kechin.vv <kechinv1@rambler.ru>
2022-12-05 00:41:36 +0400

    io files -> nio files update build: building root or every dir at one level below the root

--- src/main/kotlin/me/valer/ktlibminer/PrjBuilder.kt
+++ src/main/kotlin/me/valer/ktlibminer/PrjBuilder.kt
@@ -4,11 +4,14 @@ import me.valer.ktlibminer.repository.LocalRepository
 import org.apache.maven.shared.verifier.Verifier
 import org.gradle.tooling.GradleConnector
 import java.io.File
+import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.Paths
+
 /***
  * With maven_path use installed maven, without maven_path use default path to maven
  * With gradle_path use installed gradle, without gradle_path gradle.tooling auto download version from
- * gradle_version or necessary for project
+ * gradle_version or necessary for project (last is recommended, but it requires memory)
  ***/
 class PrjBuilder(var maven_path: Path?, var gradle_path: Path? = null, var gradle_version: String? = null) {
 
@@ -19,51 +22,61 @@ class PrjBuilder(var maven_path: Path?, var gradle_path: Path? = null, var gradl
 
     constructor(gradle_version: String) : this(null, null, gradle_version)
 
-    fun build(prj: LocalRepository) {
-        val files = File(prj.path.toString()).listFiles().filter { it.isFile }.map { it.name }
-        if (files.contains("pom.xml")) buildMaven(prj)
-        else if (files.contains("build.gradle.kts") || files.contains("build.gradle")) buildGradle(prj)
+    fun build(prj: LocalRepository): Boolean {
+        var successBuild = buildDir(prj.path)
+        if (!successBuild) successBuild = scanAndBuild(prj)
+        return successBuild
     }
 
-    fun buildGradle(prj: LocalRepository) {
+    private fun buildDir(dir: Path): Boolean {
+        return if (Files.exists(Paths.get("$dir/pom.xml"))) buildMaven(dir)
+        else if (Files.exists(Paths.get("$dir/build.gradle.kts")) || Files.exists(Paths.get("$dir/build.gradle"))) buildGradle(dir)
+        else false
+    }
+
+    /*
+    * Find pom or gradle file on 1 level below the root
+    * */
+    private fun scanAndBuild(prj: LocalRepository): Boolean {
+        var res = false
+        Files.list(prj.path).filter { Files.isDirectory(it) }.forEach {
+            val built = buildDir(it)
+            if (built) res = true
+        }
+        return res
+    }
+
+    private fun buildGradle(dir: Path): Boolean {
         val connector = GradleConnector.newConnector()
         if (gradle_path != null) connector.useInstallation(gradle_path!!.toFile())
         else if (gradle_version != null) connector.useGradleVersion(gradle_version)
-        connector.forProjectDirectory(File(prj.path.toString()))
-        connector.connect().use {
-            val build = it.newBuild()
-            build.forTasks("build")
-            build.run()
+        connector.forProjectDirectory(File(dir.toString()))
+        return try {
+            connector.connect().use {
+                val build = it.newBuild()
+                build.forTasks("build")
+                build.run()
+            }
+            true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
         }
     }
 
-    fun buildMaven(prj: LocalRepository) {
-        val baseDir = prj.path.toString()
-        try {
+    private fun buildMaven(dir: Path): Boolean {
+        val baseDir = dir.toString()
+        return try {
             val v = Verifier(baseDir)
             v.setEnvironmentVariable("maven.multiModuleProjectDirectory", baseDir)
             if (maven_path != null) v.setLocalRepo(maven_path.toString())
             v.addCliArguments("package")
             v.execute()
+            true
         } catch (e: Exception) {
             e.printStackTrace()
+            false
         }
     }
-    /*
-        fun buildMaven(prj: LocalRepository) {
-            val connector = GradleConnector.newConnector()
-            if (gradle_path != null) connector.useInstallation(gradle_path!!.toFile())
-            else if (gradle_version != null) connector.useGradleVersion(gradle_version)
-            connector.forProjectDirectory(File(prj.path.toString()))
-            connector.connect().use {
-                val build = it.newBuild()
-                build.forTasks("init")
-                build.run()
-                build.forTasks("classes")
-                build.run()
-            }
-        }
-
-    */
 }
 

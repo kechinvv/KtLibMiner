3f39876aac551df519b70eb01b14e4aad8b24473
kechin.vv <kechinv1@rambler.ru>
2023-05-22 21:35:46 +0400

    toJson update

--- src/main/kotlin/me/valer/ktlibminer/inference/FSM.kt
+++ src/main/kotlin/me/valer/ktlibminer/inference/FSM.kt
@@ -1,15 +1,31 @@
 package me.valer.ktlibminer.inference
 
+import com.google.gson.GsonBuilder
 import guru.nidi.graphviz.model.Link
 import guru.nidi.graphviz.model.MutableNode
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardOpenOption
 
-class JsonFSM(val className: String, val edgesDot: Collection<Link>, val nodesDot: Collection<MutableNode>) {
+class FSM(val className: String, val edgesDot: Collection<Link>, val nodesDot: Collection<MutableNode>) {
     val states = HashSet<State>()
     val shifts = HashSet<Shift>()
     private val initial = "initial"
     private val finish = "doublecircle"
 
-    init {
+
+    fun toJson(filePath: Path) {
+        extractData()
+        val map = HashMap<String, Any>()
+        map["name"] = className
+        map["shifts"] = shifts
+        map["states"] = states
+        val strJson = GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create().toJson(map)
+        Files.write(filePath, listOf(strJson), StandardCharsets.UTF_8, StandardOpenOption.APPEND)
+    }
+
+    fun extractData() {
         nodesDot.forEach {
             if (it.name().toString() == initial) {
                 states.add(State(initial, StateType.INIT))
@@ -32,10 +48,11 @@ class JsonFSM(val className: String, val edgesDot: Collection<Link>, val nodesDo
 
     }
 
-    data class Shift(val from: String, val to: String, val with: List<String>){
+    data class Shift(val from: String, val to: String, val with: List<String>) {
         override fun toString(): String = "{from: ${from}, to: ${to}, with: ${with}}"
     }
-    data class State(val name: String, val type: StateType){
+
+    data class State(val name: String, val type: StateType) {
         override fun toString(): String = "{name: ${name}, type: ${type}}"
     }
 }

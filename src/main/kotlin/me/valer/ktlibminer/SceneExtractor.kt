6e7a29dafd83185c7b37ec02727ad0d9f0bab70e
kechin.vv <kechinv1@rambler.ru>
2023-05-30 04:01:38 +0400

    optimize pointsTo -> store only extracted traces

--- src/main/kotlin/me/valer/ktlibminer/SceneExtractor.kt
+++ src/main/kotlin/me/valer/ktlibminer/SceneExtractor.kt
@@ -72,12 +72,11 @@ class SceneExtractor(var lib: String) {
             }))
     }
 
-
     fun graphTraverseLib(
         startPoint: Unit,
         ttl: Int = Configurations.traversJumps,
         isMethod: Boolean = false,
-        trace: ArrayDeque<InvokeExpr> = ArrayDeque(),
+        extracted: HashMap<String, MutableList<MutableList<InvokeExpr>>> = HashMap(),
         continueStack: ArrayDeque<Pair<Unit, Boolean>> = ArrayDeque()
     ) {
         val currentSuccessors = icfg.getSuccsOf(startPoint)
@@ -85,18 +84,20 @@ class SceneExtractor(var lib: String) {
             if (ttl <= 0 || !isMethod) {
                 counter++
                 if (counter % 50000 == 0) println(counter)
+                save(extracted)
                 //println("TTL = $ttl ; fullSize = ${trace.size}  ; counter = $counter")
-                extractAndSave(trace)
+//                extractAndSave(trace)
             } else {
                 val succInfo = continueStack.removeLast()
-                graphTraverseLib(succInfo.first, ttl - 1, succInfo.second, trace, continueStack)
+                graphTraverseLib(succInfo.first, ttl - 1, succInfo.second, extracted, continueStack)
                 continueStack.add(succInfo)
             }
         } else {
             for (succ in currentSuccessors) {
                 var method: SootMethod? = null
-                var invAdded = false
                 var continueAdded = false
+                var klass: String? = null
+                var addedIndexes: List<Int>? = null
                 try {
                     if (succ is JInvokeStmt || succ is JAssignStmt) {
                         succ as AbstractStmt
@@ -104,8 +105,10 @@ class SceneExtractor(var lib: String) {
                             method = succ.invokeExpr.method
                         if (foundLib(succ.invokeExpr.method)) {
                             saveMethod(succ.invokeExpr)
-                            trace.add(succ.invokeExpr)
-                            invAdded = true
+                            val decl = succ.invokeExpr.method.declaringClass
+                            klass = if (decl.isStatic) "${decl}__s" else decl.toString()
+                            if (extracted[klass] == null) extracted[klass] = mutableListOf()
+                            addedIndexes = fillExtracted(succ.invokeExpr, extracted[klass]!!)
                         }
                     }
                 } catch (_: Exception) {
@@ -114,98 +117,211 @@ class SceneExtractor(var lib: String) {
                     continueStack.add(Pair(succ, isMethod))
                     continueAdded = true
                     icfg.getStartPointsOf(method).forEach { methodStart ->
-                        graphTraverseLib(methodStart, ttl - 1, true, trace, continueStack)
+                        graphTraverseLib(methodStart, ttl - 1, true, extracted, continueStack)
                     }
-                } else graphTraverseLib(succ, ttl - 1, isMethod, trace, continueStack)
-                if (invAdded) trace.removeLast()
+                } else graphTraverseLib(succ, ttl - 1, isMethod, extracted, continueStack)
+                if (!addedIndexes.isNullOrEmpty()) {
+                    confiscate(addedIndexes, extracted[klass]!!)
+                }
                 if (continueAdded) continueStack.removeLast()
             }
         }
     }
 
 
-    private fun foundLib(method: SootMethod): Boolean {
-        return method.declaringClass.toString().startsWith("$lib.") || method.declaringClass.toString() == lib
-    }
-
-    fun sequenceExtracting(trace: List<InvokeExpr>): HashSet<List<InvokeExpr>> {
-        val extractedTracesRet = HashSet<List<InvokeExpr>>(mutableListOf())
-        if (trace.size < 2) return extractedTracesRet
-
-        val (traceStatic, traceDef) = trace.partition { it.method.isStatic }
-
-        val staticExtracted = staticExtracting(traceStatic.toMutableList())
-        extractedTracesRet.addAll(staticExtracted)
-        val invokeExtracted = defaultExtracting(traceDef.toMutableList())
-        extractedTracesRet.addAll(invokeExtracted)
-
-        return extractedTracesRet
-    }
-
-    private fun staticExtracting(traceStatic: MutableList<InvokeExpr>): HashSet<MutableList<InvokeExpr>> {
-        val extractedTracesRet = HashSet<MutableList<InvokeExpr>>()
-        traceStatic.forEach { inv ->
-            val invClass = inv.method.declaringClass
-            var inserted = false
-            extractedTracesRet.forEach {
-                val sameClass = invClass == it.last().method.declaringClass
-                if (sameClass) {
-                    it.add(inv)
-                    inserted = true
-                }
-            }
-            if (!inserted) extractedTracesRet.add(mutableListOf(inv))
+    private fun fillExtracted(invoke: InvokeExpr, extractedKlass: MutableList<MutableList<InvokeExpr>>): List<Int> {
+        return if (invoke.method.isStatic) {
+            if (extractedKlass.size != 0) extractedKlass[0].add(invoke)
+            else extractedKlass.add(mutableListOf(invoke))
+            listOf(0)
+        } else {
+            defaultExtracting(invoke, extractedKlass)
         }
-        return extractedTracesRet
     }
 
-    private fun defaultExtracting(traceDef: MutableList<InvokeExpr>): HashSet<MutableList<InvokeExpr>> {
-        val extractedTracesRet = HashSet<MutableList<InvokeExpr>>()
-        traceDef.forEach { inv ->
-            val obj1PT = getPointsToSet(inv)
-            val invClass = inv.method.declaringClass
-            var inserted = false
-            extractedTracesRet.forEach {
-                val obj2PT = getPointsToSet(it.last())
-                val sameClass = invClass == it.last().method.declaringClass
-                if (obj1PT.hasNonEmptyIntersection(obj2PT) && sameClass) {
-                    it.add(inv)
-                    inserted = true
-                }
-            }
-            if (!inserted) extractedTracesRet.add(mutableListOf(inv))
+    private fun confiscate(indexes: List<Int>, extractedKlass: MutableList<MutableList<InvokeExpr>>) {
+        indexes.forEach {
+            extractedKlass[it].removeLast()
+            if (extractedKlass[it].isEmpty()) extractedKlass.removeAt(it)
         }
-        return extractedTracesRet
+
     }
+
+    private fun save(extracted: HashMap<String, MutableList<MutableList<InvokeExpr>>>) {
+        extracted.forEach { (key, value) ->
+            value.forEach inner@{
+                if (it.size < 2) return@inner
+                val jsonData = GsonBuilder().disableHtmlEscaping().create().toJson(it.map { invoke ->
+                    if (Configurations.traceNode == TraceNode.NAME) invoke.method.name
+                    else invoke.method.signature.replace(' ', '+')
+                })
+                DatabaseController.addTrace(jsonData!!, key)
+            }
+        }
+    }
+
+
+    private fun defaultExtracting(invoke: InvokeExpr, extractedKlass: MutableList<MutableList<InvokeExpr>>): List<Int> {
+        val indexes = mutableListOf<Int>()
+        val obj1PT = getPointsToSet(invoke)
+        var inserted = false
+        extractedKlass.forEachIndexed { index, it ->
+            if (it.isEmpty()) return@forEachIndexed
+            val obj2PT = getPointsToSet(it.last())
+            if (obj1PT.hasNonEmptyIntersection(obj2PT)) {
+                indexes.add(index)
+                it.add(invoke)
+                inserted = true
+            }
+        }
+        if (!inserted) {
+            extractedKlass.add(mutableListOf(invoke))
+            indexes.add(extractedKlass.lastIndex)
+        }
+        return indexes
+    }
+
+    private fun saveMethod(invoke: InvokeExpr) {
+        val name = if (Configurations.traceNode == TraceNode.NAME) invoke.method.name
+        else invoke.method.signature.replace(' ', '+')
+        val klass =
+            if (invoke.method.isStatic) "${invoke.method.declaringClass}__s" else invoke.method.declaringClass.toString()
+        DatabaseController.addMethod(
+            name,
+            klass
+        )
+    }
+
 
     private fun getPointsToSet(inv: InvokeExpr): PointsToSet {
         return analysis.reachingObjects(inv.useBoxes[0].value as Local)
     }
 
-
-    private fun extractAndSave(trace: List<InvokeExpr>) {
-            val extractedTraces = sequenceExtracting(trace).filter { it.size > 1 }
-            extractedTraces.forEach {
-                val indicator = it.first()
-                var klass = indicator.method.declaringClass.toString()
-                if (indicator.method.isStatic) klass += "__s"
-                val jsonData = GsonBuilder().disableHtmlEscaping().create().toJson(it.map { invoke ->
-                    if (Configurations.traceNode == TraceNode.NAME) invoke.method.name
-                    else invoke.method.signature.replace(' ', '+')
-                })
-                DatabaseController.addTrace(jsonData!!, klass)
-            }
+    private fun foundLib(method: SootMethod): Boolean {
+        return method.declaringClass.toString().startsWith("$lib.") || method.declaringClass.toString() == lib
     }
 
-    private fun saveMethod(invoke: InvokeExpr) {
-            val name = if (Configurations.traceNode == TraceNode.NAME) invoke.method.name
-            else invoke.method.signature.replace(' ', '+')
-            val klass =
-                if (invoke.method.isStatic) "${invoke.method.declaringClass}__s" else invoke.method.declaringClass.toString()
-            DatabaseController.addMethod(
-                name,
-                klass
-            )
-    }
+//    fun graphTraverseLib(
+//        startPoint: Unit,
+//        ttl: Int = Configurations.traversJumps,
+//        isMethod: Boolean = false,
+//        trace: ArrayDeque<InvokeExpr> = ArrayDeque(),
+//        continueStack: ArrayDeque<Pair<Unit, Boolean>> = ArrayDeque()
+//    ) {
+//        val currentSuccessors = icfg.getSuccsOf(startPoint)
+//        if (currentSuccessors.size == 0 || ttl <= 0) {
+//            if (ttl <= 0 || !isMethod) {
+//                counter++
+//                if (counter % 50000 == 0) println(counter)
+//                //println("TTL = $ttl ; fullSize = ${trace.size}  ; counter = $counter")
+//                extractAndSave(trace)
+//            } else {
+//                val succInfo = continueStack.removeLast()
+//                graphTraverseLib(succInfo.first, ttl - 1, succInfo.second, trace, continueStack)
+//                continueStack.add(succInfo)
+//            }
+//        } else {
+//            for (succ in currentSuccessors) {
+//                var method: SootMethod? = null
+//                var invAdded = false
+//                var continueAdded = false
+//                try {
+//                    if (succ is JInvokeStmt || succ is JAssignStmt) {
+//                        succ as AbstractStmt
+//                        if (succ.invokeExpr.method.declaringClass in Scene.v().applicationClasses)
+//                            method = succ.invokeExpr.method
+//                        if (foundLib(succ.invokeExpr.method)) {
+//                            saveMethod(succ.invokeExpr)
+//                            trace.add(succ.invokeExpr)
+//                            invAdded = true
+//                        }
+//                    }
+//                } catch (_: Exception) {
+//                }
+//                if (method != null && method.declaringClass in Scene.v().applicationClasses) {
+//                    continueStack.add(Pair(succ, isMethod))
+//                    continueAdded = true
+//                    icfg.getStartPointsOf(method).forEach { methodStart ->
+//                        graphTraverseLib(methodStart, ttl - 1, true, trace, continueStack)
+//                    }
+//                } else graphTraverseLib(succ, ttl - 1, isMethod, trace, continueStack)
+//                if (invAdded) trace.removeLast()
+//                if (continueAdded) continueStack.removeLast()
+//            }
+//        }
+//    }
+
+
+//    private fun extractAndSave(trace: List<InvokeExpr>) {
+//        val extractedTraces = sequenceExtracting(trace).filter { it.size > 1 }
+//        save(extractedTraces)
+//    }
+//
+//    private fun save(extracted: List<List<InvokeExpr>>) {
+//        extracted.forEach {
+//            if (it.size < 2) return@forEach
+//            val indicator = it.first().method
+//            var klass = indicator.declaringClass.toString()
+//            if (indicator.isStatic) klass += "__s"
+//            val jsonData = GsonBuilder().disableHtmlEscaping().create().toJson(it.map { invoke ->
+//                if (Configurations.traceNode == TraceNode.NAME) invoke.method.name
+//                else invoke.method.signature.replace(' ', '+')
+//            })
+//            DatabaseController.addTrace(jsonData!!, klass)
+//        }
+//    }
+
+
+//    fun sequenceExtracting(trace: List<InvokeExpr>): HashSet<List<InvokeExpr>> {
+//        val extractedTracesRet = HashSet<List<InvokeExpr>>(mutableListOf())
+//        if (trace.size < 2) return extractedTracesRet
+//
+//        val (traceStatic, traceDef) = trace.partition { it.method.isStatic }
+//
+//        val staticExtracted = staticExtracting(traceStatic.toMutableList())
+//        extractedTracesRet.addAll(staticExtracted)
+//        val invokeExtracted = defaultExtracting(traceDef.toMutableList())
+//        extractedTracesRet.addAll(invokeExtracted)
+//
+//        return extractedTracesRet
+//    }
+
+
+//    private fun staticExtracting(traceStatic: MutableList<InvokeExpr>): HashSet<MutableList<InvokeExpr>> {
+//        val extractedTracesRet = HashSet<MutableList<InvokeExpr>>()
+//        traceStatic.forEach { inv ->
+//            val invClass = inv.method.declaringClass
+//            var inserted = false
+//            extractedTracesRet.forEach {
+//                val sameClass = invClass == it.last().method.declaringClass
+//                if (sameClass) {
+//                    it.add(inv)
+//                    inserted = true
+//                }
+//            }
+//            if (!inserted) extractedTracesRet.add(mutableListOf(inv))
+//        }
+//        return extractedTracesRet
+//    }
+//
+//    private fun defaultExtracting(traceDef: MutableList<InvokeExpr>): HashSet<MutableList<InvokeExpr>> {
+//        val extractedTracesRet = HashSet<MutableList<InvokeExpr>>()
+//        traceDef.forEach { inv ->
+//            val obj1PT = getPointsToSet(inv)
+//            val invClass = inv.method.declaringClass
+//            var inserted = false
+//            extractedTracesRet.forEach {
+//                val obj2PT = getPointsToSet(it.last())
+//                val sameClass = invClass == it.last().method.declaringClass
+//                if (obj1PT.hasNonEmptyIntersection(obj2PT) && sameClass) {
+//                    it.add(inv)
+//                    inserted = true
+//                }
+//            }
+//            if (!inserted) extractedTracesRet.add(mutableListOf(inv))
+//        }
+//        return extractedTracesRet
+//    }
+
 
 }
